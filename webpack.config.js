b26aa65fb93dbd5e185ad376b0c7bdfcb208a256
Slava Dmitriev <d2c.tool@gmail.com>
2022-01-16 17:33:14 +0300

    fixed webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
